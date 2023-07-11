41371b471077a443f8c6934ba7c123427f728d10
luucasor <lucas@jornadatecnologia.com>
2023-07-11 20:38:23 -0300

    Caso você crie uma nova variável em TS e não passe o tipo, o tipo é inferidos pelo compilador. Mais exemplos com numbers e boolean

--- tipos/tipo.ts
+++ tipos/tipo.ts
@@ -2,3 +2,14 @@
 let nome = 'João'
 console.log(nome)
 //nome = 28
+
+// numbers
+let idade = 27
+//idade = 'Ana'
+idade = 27.5  //javascript não faz diferenciação entre tipos inteiros ou flutuantes
+console.log(idade)
+
+// boolean
+let possuiHobbies = false
+// possuiHobbies = 1
+console.log(possuiHobbies)
