27237936031665d88dc171623dea4aa1c9ad9c46
luucasor <lucas@jornadatecnologia.com>
2023-07-11 21:30:12 -0300

    Exemplo com o uso de classe implementando interface e declaração de função com parâmetros obrigatórios

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -41,3 +41,18 @@ meuCliente.nome = 'Han'
 saudarComOla(meuCliente)
 meuCliente.saudar('Solo')
 console.log(meuCliente.ultimaCompra)
+
+// Interface Função
+interface FuncaoCalculo {
+    (a: number, b: number): number
+}
+
+let potencia: FuncaoCalculo
+
+potencia = function(base: number, exp: number): number { 
+    return Array(exp).fill(base).reduce((t, a) => t * a)
+}
+
+console.log(Math.pow(3, 10))
+console.log(3 ** 10)
+console.log(potencia(3, 10))
